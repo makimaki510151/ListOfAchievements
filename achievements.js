f2b0104fa49a46ddaecea861ab202aba1bed6ee8
makimaki510151 <rsv95076@gmail.com>
2025-10-14 17:10:04 +0900

    idの大幅変更

--- achievements.js
+++ achievements.js
@@ -7,32 +7,34 @@ const achievementsData = [
     {
         category: "日常",
         items: [
-            { id: "mission-1-1", name: "二十歳になる" },
-            { id: "mission-1-2", name: "1時間散歩する" },
-            { id: "mission-1-3", name: "予定より1本前の電車に乗る" },
-            { id: "mission-1-4", name: "電車の扉が目の前で閉まり乗り損ねる" }
+            { id: "1-1", name: "二十歳になる" },
+            { id: "1-2", name: "1時間散歩する" },
+            { id: "1-3", name: "予定より1本前の電車に乗る" },
+            { id: "1-4", name: "電車の扉が目の前で閉まり乗り損ねる" }
         ]
     },
     {
         category: "運動",
         items: [
-            { id: "challenge-2-1", name: "バク転を成功させる" },
-            { id: "challenge-2-2", name: "30kgのお米を持つ" },
-            { id: "challenge-2-3", name: "何かを買いにコンビニまでダッシュする" }
+            { id: "2-1", name: "バク転を成功させる" },
+            { id: "2-2", name: "30kgのお米を持つ" },
+            { id: "2-3", name: "何かを買いにコンビニまでダッシュする" }
         ]
     },
     {
         category: "食事",
         items: [
-            { id: "master-3-1", name: "次郎系ラーメンを食べる" },
-            { id: "master-3-2", name: "ピザを1枚一人で食べる" },
-            { id: "master-3-3", name: "朝昼夜の食事をコンビニで済ませる" },
+            { id: "3-1", name: "次郎系ラーメンを食べる" },
+            { id: "3-2", name: "ピザを1枚一人で食べる" },
+            { id: "3-3", name: "朝昼夜の食事をコンビニで済ませる" },
         ]
     },
     {
         category: "その他",
         items: [
-            { id: "master-99-1", name: "シャベルで穴を掘る" }
+            { id: "99-1", name: "シャベルで穴を掘る" },
+            { id: "99-2", name: "PCを自分で稼いだお金で購入する" },
+            { id: "99-3", name: "お金を稼いだ" }
         ]
     },
 ];
